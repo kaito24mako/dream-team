7186218ea74f71cedc85252ef626a6ff0ee88237
Kaito Watanabe <kaitowatanabemcc@gmail.com>
2026-08-06 11:32:54 +1000

    fix Button "undefined" error by providing default values in Button component

--- client/src/components/common/button/Button.tsx
+++ client/src/components/common/button/Button.tsx
@@ -1,6 +1,6 @@
-type Size = "xs" | "small";
-type BgColor = "primary" | "secondary" | "accent";
-type TextColor = "white" | "black";
+type Size = "default" | "xs" | "small";
+type BgColor = "default" | "primary" | "secondary" | "accent";
+type TextColor = "default" | "white" | "black";
 
 type Props = {
   children: React.ReactNode;
@@ -9,17 +9,25 @@ type Props = {
   textColor?: TextColor;
 };
 
-function Button({ children, size, bgColor, textColor }: Props) {
+function Button({
+  children,
+  size = "default",
+  bgColor = "default",
+  textColor = "default",
+}: Props) {
   const sizeClasses: Record<Size, string> = {
+    default: "",
     xs: "btn-xs",
     small: "btn-sm",
   };
   const bgColorClasses: Record<BgColor, string> = {
+    default: "",
     primary: "btn-primary",
     secondary: "btn-secondary",
     accent: "btn-accent",
   };
   const textColorClasses: Record<TextColor, string> = {
+    default: "",
     white: "text-white",
     black: "text-black",
   };
@@ -27,7 +35,7 @@ function Button({ children, size, bgColor, textColor }: Props) {
   return (
     <button
       type="button"
-      className={`btn ${`size ? ${sizeClasses[size]} : ""`} ${`bgColor ? ${bgColorClasses[bgColor]} : ""`} ${`textColor ? ${textColorClasses[textColor]} : ""`}`}
+      className={`btn ${sizeClasses[size]} ${bgColorClasses[bgColor]} ${textColorClasses[textColor]}`}
     >
       {children}
     </button>
